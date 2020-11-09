8ee75510abd665219be4f725e483ce6137fcabe0
royalhope <70911264+royalhope@users.noreply.github.com>
2020-11-09 19:29:01 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format : 'png',
+    png_quality:90,
+
+    constraints: {
+        facingMode: "environment"
+    }
+});
+
+camera = document.getElementById('camera');
+
+Webcam.attach( '#camera' );
