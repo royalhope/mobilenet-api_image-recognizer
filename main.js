fb60c05906ea2b67d5562bdbd5a66d57dbeecc0c
royalhope <70911264+royalhope@users.noreply.github.com>
2020-11-15 15:30:02 +0530

    Completed Project

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ Webcam.set({
 camera = document.getElementById('camera');
 
 Webcam.attach( '#camera' );
+
+function take_snapshot() {
+        Webcam.snap(function(data_uri) {
+            document.getElementById('result').innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded() {
+    console.log('model loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        alert(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
